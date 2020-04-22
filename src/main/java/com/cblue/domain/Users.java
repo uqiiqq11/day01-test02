0b7ad1a72486e22931cd655d1dac3b97267681ee
uqiiqq11 <1170546314@qq.com>
2020-04-22 11:12:36 +0800

    添加了私有化成员变量

--- src/main/java/com/cblue/domain/Users.java
+++ src/main/java/com/cblue/domain/Users.java
@@ -3,11 +3,21 @@ package com.cblue.domain;
 public class Users {
     private Integer id;
 
+    private String name;
+
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
